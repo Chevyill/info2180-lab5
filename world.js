5462d91ea29ce480165be118d2c1aa9325b1e42a
Chevyill <149888175+Chevyill@users.noreply.github.com>
2024-12-01 23:51:21 -0500

    Add files via upload

---
+++ world.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function fetchData(endpoint) {
+        const country = document.getElementById("country").value.trim();
+
+        if (country === "") {
+            document.getElementById("result").innerHTML = "<p>Please enter a country name.</p>"; // Message incase the user eenters a a blank text.
+            return;
+        }
+
+        // Sets up the sending of HTTP requests to the server.
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", `${endpoint}${encodeURIComponent(country)}`, true);
+
+        xhr.onload = function() {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                const response = xhr.responseText;
+                document.getElementById("result").innerHTML = response;
+            } else {
+                document.getElementById("result").innerHTML = "<p>Error fetching data.</p>";
+            }
+        };
+
+        xhr.onerror = function() {
+            document.getElementById("result").innerHTML = "<p>Network error. Please try again later.</p>";
+        };
+        xhr.send();
+    }
+
+    // Adds an event to the lookup button, shows result based on the endpoint.
+    document.getElementById("lookup").addEventListener("click", function() {
+        fetchData(`world.php?country=`);
+    });
+
+    // Adds an event to the lookupCities button
+    document.getElementById("lookupCities").addEventListener("click", function() {
+        fetchData(`world.php?lookup=cities&country=`);
+    });
+});   
